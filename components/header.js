94d67ae2f9e2503078e3826d289339ae913af9aa
idei000sports <idei000sports@gmail.com>
2023-07-20 10:08:11 +0900

    <a>を<Link>に修正した

--- components/header.js
+++ components/header.js
@@ -1,9 +1,10 @@
 import styles   from '../styles/Header.module.css'
+import Link from 'next/link'
 
 export default function Header(){
     return (
         <>
-			<h1 className={styles.site_title}><a href="/">漫画蔵</a></h1>
+			<h1 className={styles.site_title}><Link href="/">漫画蔵</Link></h1>
 			<p className={styles.explanation}>漫画</p>
                         
         </>
